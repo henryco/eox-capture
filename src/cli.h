ad3d79537775f2ab062fbe4d1cb939b5e1791b56
Henry Domagalski <henrycodev@gmail.com>
2023-12-25 21:25:32 +0100

    added glm and argparse libs

--- src/cli.h
+++ src/cli.h
@@ -5,7 +5,7 @@
 #ifndef STEREOX_CLI_H
 #define STEREOX_CLI_H
 
-#include "argparse/argparse.hpp"
+#include <argparse/argparse.hpp>
 #include <opencv2/videoio.hpp>
 #include <spdlog/spdlog.h>
 #include "aux/commons.h"
